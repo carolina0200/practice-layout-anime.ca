9a73d822c1122c9f1a8d42dd1216129fd27c1894
Carolina Giraldo <carolinagiraldo@saimyr.com.co>
2019-02-07 11:21:37 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './general/first/first.component';
+import { TabsComponent } from './general/tabs/tabs.component';
+import { ContentComponent } from './general/content/content.component';
+import { HeaderComponent } from './general/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    TabsComponent,
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
